9386d01ec3853c5b95c3b3be0e305e6dbae72960
Paulo Feitosa <coding.paulo.feitosa@gmail.com>
2017-04-08 07:50:50 -0300

    added interface definition

--- transparent-mode.sh
+++ transparent-mode.sh
@@ -12,7 +12,7 @@ set -o nounset
 #set -o xtrace
 
 usage() {
-    printf "%s\n" "Usage: $0 [options]" >&2
+    printf "%s\n" "Usage: $0 [options] <interface>" >&2
     printf "%s\n\n" "Options:" >&2
     printf "%s\t%s\n" "-h" "Show this message and exit." >&2
     printf "%s\t%s\n" "-p" "Proxy service port." >&2
@@ -24,6 +24,12 @@ main() {
     exit 1
   fi
 
+  if [ "$#" -lt 1 ]; then
+    usage
+    exit 1
+  fi
+
+  local iface="$1"
   local port="8080"
 
   while getopts "h?p:" opt; do
@@ -44,8 +50,8 @@ main() {
 
   "${sysctl}" -w net.ipv4.ip_forward=1
   echo 0 | tee /proc/sys/net/ipv4/conf/*/send_redirects
-  "${iptables}" -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port "${port}"
-  "${iptables}" -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port "${port}"
+  "${iptables}" -t nat -A PREROUTING -i "${iface}" -p tcp --dport 80 -j REDIRECT --to-port "${port}"
+  "${iptables}" -t nat -A PREROUTING -i "${iface}" -p tcp --dport 443 -j REDIRECT --to-port "${port}"
 }
 
 main "$@"
